de76dff462fdddb23fcefe451f00bae0628e945b
Pekka Hyvönen <pekka@vaadin.com>
2018-03-21 21:28:01 +0200

    Enable buttons in tutorial only when needed

--- tutorial-getting-started/src/main/java/com/vaadin/flow/demo/helloworld/CustomerForm.java
+++ tutorial-getting-started/src/main/java/com/vaadin/flow/demo/helloworld/CustomerForm.java
@@ -11,49 +11,57 @@ import com.vaadin.flow.tutorial.annotations.CodeFor;
 @SuppressWarnings("serial")
 @CodeFor("introduction/tutorial-get-started.asciidoc")
 public class CustomerForm extends FormLayout {
-	private TextField firstName = new TextField("First name");
-	private TextField lastName = new TextField("Last name");
-	private ComboBox<CustomerStatus> status = new ComboBox<>("Status");
-	private Button save = new Button("Save");
-	private Button delete = new Button("Delete");
+    private TextField firstName = new TextField("First name");
+    private TextField lastName = new TextField("Last name");
+    private ComboBox<CustomerStatus> status = new ComboBox<>("Status");
+    private Button save = new Button("Save");
+    private Button delete = new Button("Delete");
 
-	private CustomerService service = CustomerService.getInstance();
-	private Customer customer;
-	private MainView view;
-	
-	private Binder<Customer> binder = new Binder<>(Customer.class);
+    private CustomerService service = CustomerService.getInstance();
+    private Customer customer;
+    private MainView view;
 
-	public CustomerForm(MainView view) {
-	    this.view = view;
+    private Binder<Customer> binder = new Binder<>(Customer.class);
 
-	    HorizontalLayout buttons = new HorizontalLayout(save, delete);
-	    
-	    add(firstName, lastName, status, buttons);
-	    
-	    status.setItems(CustomerStatus.values());
+    public CustomerForm(MainView view) {
+        this.view = view;
 
-	    save.getElement().setAttribute("theme", "primary");
-	    
-	    binder.bindInstanceFields(this);
-	    
-	    save.addClickListener(e -> this.save());
-	    delete.addClickListener(e -> this.delete());
-	}
-	
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	    binder.setBean(customer);
-	}
-	
-	private void delete() {
-	    service.delete(customer);
-	    view.updateList();
-	    setCustomer(null);
-	}
+        HorizontalLayout buttons = new HorizontalLayout(save, delete);
 
-	private void save() {
-	    service.save(customer);
-	    view.updateList();
-	    setCustomer(null);
-	}
+        add(firstName, lastName, status, buttons);
+
+        status.setItems(CustomerStatus.values());
+
+        save.getElement().setAttribute("theme", "primary");
+
+        binder.bindInstanceFields(this);
+
+        save.addClickListener(e -> this.save());
+        delete.addClickListener(e -> this.delete());
+
+        setCustomer(null);
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+        binder.setBean(customer);
+        boolean enabled = customer != null;
+        save.setEnabled(enabled);
+        delete.setEnabled(enabled);
+        if (enabled) {
+            firstName.focus();
+        }
+    }
+
+    private void delete() {
+        service.delete(customer);
+        view.updateList();
+        setCustomer(null);
+    }
+
+    private void save() {
+        service.save(customer);
+        view.updateList();
+        setCustomer(null);
+    }
 }
