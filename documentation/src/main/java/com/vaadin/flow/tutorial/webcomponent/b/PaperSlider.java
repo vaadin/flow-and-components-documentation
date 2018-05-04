f84657bf35209763ed4b82ae483c721a588b148a
Leif Åstrand <legioth@gmail.com>
2018-05-04 08:41:50 +0300

    Update the HasValue part of the web component documentation (#107)
    * Update the HasValue part of the web component documentation
    
    * Merge branch 'master' into webComponentHasValue
    
    * Merge branch 'master' into webComponentHasValue

--- documentation/src/main/java/com/vaadin/flow/tutorial/webcomponent/b/PaperSlider.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/webcomponent/b/PaperSlider.java
@@ -1,26 +1,26 @@
 package com.vaadin.flow.tutorial.webcomponent.b;
 
 import com.vaadin.flow.component.AbstractSinglePropertyField;
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEventListener;
-import com.vaadin.flow.component.HasValue;
 import com.vaadin.flow.component.PropertyDescriptor;
 import com.vaadin.flow.component.PropertyDescriptors;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.shared.Registration;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 import com.vaadin.flow.tutorial.webcomponent.ClickEvent;
 
 @CodeFor("web-components/creating-java-api-for-a-web-component.asciidoc")
 //@formatter:off
+@Tag("paper-slider")
+@HtmlImport("bower_components/paper-slider/paper-slider.html")
 public class PaperSlider extends AbstractSinglePropertyField<PaperSlider, Integer> {
-    private static final PropertyDescriptor<Integer, Integer> valueProperty = PropertyDescriptors.propertyWithDefault("value", 0);
+
+    private static final PropertyDescriptor<Boolean, Boolean> pinProperty = PropertyDescriptors.propertyWithDefault("pin", false);
 
     public PaperSlider() {
-        super("", 0, false);
+        super("value", 0, false);
     }
-
-    // @formatter:off
-    private static final PropertyDescriptor<Boolean, Boolean> pinProperty = PropertyDescriptors.propertyWithDefault("pin", false);
 
     public void setPin(boolean pin) {
         pinProperty.set(this, pin);
@@ -30,16 +30,6 @@ public class PaperSlider extends AbstractSinglePropertyField<PaperSlider, Intege
         return pinProperty.get(this);
     }
 
-    @Override
-    public void setValue(Integer value) {
-        valueProperty.set(this, value);
-    }
-
-    @Override
-    public Integer getValue() {
-        return valueProperty.get(this);
-    }
-
     public Registration addClickListener(ComponentEventListener<ClickEvent> listener) {
         return addListener(ClickEvent.class, listener);
     }
@@ -47,6 +37,5 @@ public class PaperSlider extends AbstractSinglePropertyField<PaperSlider, Intege
     public void increment() {
         getElement().callFunction("increment");
     }
-
-    //@formatter:on
 }
+//@formatter:on
