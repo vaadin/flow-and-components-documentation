f2474c4bed100fd81aeebfd844aa2bd03cc030ae
Diego Sanz Villafruela <diegosanz@vaadin.com>
2018-11-16 16:23:24 +0200

    code & link to code - servlet

---
+++ tutorial-servlet-spring-boot/src/main/java/org/vaadin/diego/spring/MainView.java
@@ -0,0 +1,20 @@
+package org.vaadin.diego.spring;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView(@Autowired MessageBean bean) {
+        Button button = new Button("Click me",
+                e -> Notification.show(bean.getMessage()));
+        add(button);
+    }
+
+
+}
