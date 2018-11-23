f88816e20e6257c09457b895196eff30ca71afde
Diego Sanz Villafruela <diegosanz@vaadin.com>
2018-11-23 11:45:18 +0200

    code for snipets

---
+++ documentation/src/main/java/com/vaadin/flow/tutorial/creatingcomponents/Tooltip.java
@@ -0,0 +1,11 @@
+package com.vaadin.flow.tutorial.creatingcomponents;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.HasComponents;
+import com.vaadin.flow.component.HasStyle;
+import com.vaadin.flow.tutorial.annotations.CodeFor;
+
+@CodeFor("creating-components/tutorial-component-mixins.asciidoc")
+public class Tooltip extends Component implements HasComponents, HasStyle {
+
+}
