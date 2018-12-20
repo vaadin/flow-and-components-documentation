2bb1562204544c3ae8da23eda4a70d27bed5ff63
Diego Sanz Villafruela <diegosanz@vaadin.com>
2018-12-20 10:55:26 +0200

    better examples - thomas' feedback

--- documentation/src/main/java/com/vaadin/flow/tutorial/element/RPC.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/element/RPC.java
@@ -1,6 +1,8 @@
 package com.vaadin.flow.tutorial.element;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.page.Page;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 
 @CodeFor("element-api/client-server-rpc.asciidoc")
@@ -8,5 +10,22 @@ public class RPC extends Component {
     public RPC() {
         getElement().callFunction("remoteProcedure","Hello","World",1.0);
         getElement().executeJavaScript("$0.remoteProcedure($1,$2,$3);",getElement(),"Hello","World",1.0);
+    }
+
+    public void clearSelection() {
+        getElement().callFunction("clearSelection");
+    }
+
+    public void setExpanded(Component component) {
+        getElement().callFunction("expand", component.getElement());
+    }
+
+    public void complete() {
+        // Previous Vaadin Platform versions
+        Page page = UI.getCurrent().getPage();
+        page.executeJavaScript("$0.complete()", this);
+
+        // Vaadin 12
+        getElement().executeJavaScript("$0.complete()", this);
     }
 }
