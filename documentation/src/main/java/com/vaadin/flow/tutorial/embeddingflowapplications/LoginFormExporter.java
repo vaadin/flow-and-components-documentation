5a1115a34b86ddea992ae93d2b222260191359aa
ZheSun88 <zhe@vaadin.com>
2019-03-15 11:55:35 +0200

    Update @Tag usage

--- documentation/src/main/java/com/vaadin/flow/tutorial/embeddingflowapplications/LoginFormExporter.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/embeddingflowapplications/LoginFormExporter.java
@@ -1,16 +1,13 @@
 package com.vaadin.flow.tutorial.embeddingflowapplications;
 
+import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.WebComponentExporter;
 import com.vaadin.flow.component.webcomponent.WebComponentDefinition;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 
 @CodeFor("embedding-flow-applications/tutorial-webcomponent-exporter.asciidoc")
+@Tag("login-form")
 public class LoginFormExporter implements WebComponentExporter<LoginForm> {
-
-    @Override
-    public String tag() {
-        return "login-form";
-    }
 
     @Override
     public void define(WebComponentDefinition<LoginForm> definition) {
