bb4eb7573965ce93d6aa459b201a23a1188ff6fe
Denis Anisimov <denis@vaadin.com>
2019-03-21 15:40:46 +0300

    Extend the SSWC tutorial with fireEvent example.
    Part of vaadin/flow#5272

--- documentation/src/main/java/com/vaadin/flow/tutorial/embeddingflowapplications/LoginForm.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/embeddingflowapplications/LoginForm.java
@@ -1,12 +1,15 @@
 package com.vaadin.flow.tutorial.embeddingflowapplications;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.function.SerializableRunnable;
 import com.vaadin.flow.server.VaadinRequest;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 
@@ -18,6 +21,7 @@ public class LoginForm extends Div {
     private String userLabel;
     private String pwdLabel;
     private FormLayout layout = new FormLayout();
+    private List<SerializableRunnable> loginListeners = new CopyOnWriteArrayList<>();
 
     public LoginForm() {
         updateForm();
@@ -45,16 +49,23 @@ public class LoginForm extends Div {
         layout.addFormItem(password, pwdLabel);
     }
 
+    public void addLoginListener(SerializableRunnable loginListener) {
+        loginListeners.add(loginListener);
+    }
+
     private void login() {
         Optional<Object> authToken = UserService.getInstance()
                 .authenticate(userName.getValue(), password.getValue());
         if (authToken.isPresent()) {
             VaadinRequest.getCurrent().getWrappedSession()
                     .setAttribute("auth_token", authToken.get());
-            getUI().get().getPage()
-                    .executeJavaScript("window.location.href='/'");
+            fireLoginEvent();
         } else {
             errorMsg.setText("Authentication failure");
         }
+    }
+
+    private void fireLoginEvent() {
+        loginListeners.forEach(SerializableRunnable::run);
     }
 }
