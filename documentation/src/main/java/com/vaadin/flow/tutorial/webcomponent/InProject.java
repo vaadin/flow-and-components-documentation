0fbc6458baf15ac417f4021c5c37379d0d73b3c3
Kirill Bulatov <mail4score@gmail.com>
2019-03-25 12:42:38 +0200

    Remove the bower and webjar references from the documentation

--- documentation/src/main/java/com/vaadin/flow/tutorial/webcomponent/InProject.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/webcomponent/InProject.java
@@ -2,14 +2,14 @@ package com.vaadin.flow.tutorial.webcomponent;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 
 @CodeFor("web-components/creating-an-in-project-web-component.asciidoc")
 public class InProject {
     @Tag("my-test-element")
-    @HtmlImport("bower_components/my-test-element/my-test-element.html")
+    @JavaScript("my-test-element/my-test-element.js")
     public class MyTest extends Component {
 
         public MyTest(String prop1) {
