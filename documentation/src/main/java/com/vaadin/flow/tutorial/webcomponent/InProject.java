8b89ab87b6a025c5864ce06469a5c46d6ff6d2f1
Kirill Bulatov <mail4score@gmail.com>
2019-03-26 09:42:43 +0200

    Replace the @JavaScript annotation with @JsModule one

--- documentation/src/main/java/com/vaadin/flow/tutorial/webcomponent/InProject.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/webcomponent/InProject.java
@@ -2,14 +2,13 @@ package com.vaadin.flow.tutorial.webcomponent;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 
 @CodeFor("web-components/creating-an-in-project-web-component.asciidoc")
 public class InProject {
     @Tag("my-test-element")
-    @JavaScript("my-test-element/my-test-element.js")
+    @JsModule("my-test-element/my-test-element.js")
     public class MyTest extends Component {
 
         public MyTest(String prop1) {
