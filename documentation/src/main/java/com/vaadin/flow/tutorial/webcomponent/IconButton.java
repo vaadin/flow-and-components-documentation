7828058fe82b517f2431b179538be0d907633e9f
Kirill Bulatov <mail4score@gmail.com>
2019-04-04 15:42:13 +0300

    Remove the bower and webjar references from the documentation (#544)
    * Remove the bower and webjar references from the documentation
    
    * Replace the @JavaScript annotation with @JsModule one
    
    * Fix the demo code
    
    * Code review fixes

--- documentation/src/main/java/com/vaadin/flow/tutorial/webcomponent/IconButton.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/webcomponent/IconButton.java
@@ -4,15 +4,18 @@ import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 
 @CodeFor("web-components/creating-java-api-for-a-web-component.asciidoc")
 @Tag("vaadin-button")
-@HtmlImport("bower_components/vaadin-button/vaadin-button.html")
-@HtmlImport("bower_components/vaadin-icons/vaadin-icons.html")
+@NpmPackage("@vaadin/vaadin-button")
+@JsModule("@vaadin/vaadin-button/vaadin-button.js")
+@NpmPackage("@vaadin/vaadin-icons")
+@JsModule("@vaadin/vaadin-icons/vaadin-icons.js")
 public class IconButton extends Component {
 
     private VaadinIcon icon;
