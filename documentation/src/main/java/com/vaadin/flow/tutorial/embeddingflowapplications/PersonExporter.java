0582636a64fb33ab81da7adcfef2e2fd00dc76cd
Joni Uitto <joni@vaadin.com>
2019-04-10 14:28:18 +0300

    Update tutorial to match abstract WebComponentExporter approach

--- documentation/src/main/java/com/vaadin/flow/tutorial/embeddingflowapplications/PersonExporter.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/embeddingflowapplications/PersonExporter.java
@@ -1,35 +1,31 @@
 package com.vaadin.flow.tutorial.embeddingflowapplications;
 
-import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.WebComponentExporter;
 import com.vaadin.flow.component.webcomponent.PropertyConfiguration;
 import com.vaadin.flow.component.webcomponent.WebComponent;
-import com.vaadin.flow.component.webcomponent.WebComponentDefinition;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 
-@CodeFor("embedding-flow-applications/tutorial-webcomponent-properties"
-        + ".asciidoc")
-@Tag("person-display")
-public class PersonExporter implements WebComponentExporter<PersonComponent> {
+@CodeFor("embedding-flow-applications/tutorial-webcomponent-properties" +
+        ".asciidoc")
+public class PersonExporter extends WebComponentExporter<PersonComponent> {
+    private PropertyConfiguration<PersonComponent, Boolean> propertyIsAdult;
 
-    private PropertyConfiguration<PersonComponent, Boolean> isAdultProperty;
-
-    @Override
-    public void define(WebComponentDefinition<PersonComponent> definition) {
-        definition.addProperty("name", "John Doe")
+    public PersonExporter() {
+        super("person-display");
+        addProperty("name", "John Doe")
                 .onChange(PersonComponent::setName);
-        definition.addProperty("age", 0).onChange(PersonComponent::setAge);
+        addProperty("age", 0)
+                .onChange(PersonComponent::setAge);
 
-        isAdultProperty = definition.addProperty("is-adult", false);
+        propertyIsAdult = addProperty("is-adult", false);
     }
 
     @Override
-    public void configure(WebComponent<PersonComponent> webComponent,
-            PersonComponent component) {
+    public void configureInstance(WebComponent<PersonComponent> webComponent, PersonComponent component) {
         component.setAdultAge(18); // initialization
 
         component.addAgeChangedListener(event -> {
-            webComponent.setProperty(isAdultProperty, component.isAdult());
+            webComponent.setProperty(propertyIsAdult, component.isAdult());
         });
 
         component.addAgeChangedListener(event -> {
