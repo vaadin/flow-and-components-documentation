c6b120696ac3f9fde32f6d160e6d8977d170d48b
Denis Anisimov <denis@vaadin.com>
2019-04-12 08:39:11 +0300

    Add a tutorial about security in embedded web applications

---
+++ documentation/src/main/java/com/vaadin/flow/tutorial/embeddingflowapplications/EmbeddedComponent.java
@@ -0,0 +1,16 @@
+package com.vaadin.flow.tutorial.embeddingflowapplications;
+
+import com.vaadin.flow.tutorial.annotations.CodeFor;
+
+@CodeFor("embedding-flow-applications/tutorial-webcomponent-security.asciidoc")
+public class EmbeddedComponent extends Div {
+
+    public EmbeddedComponent() {
+        // Don't retrieve any sensible data here without granted access
+        // (via security token)
+    }
+
+    public void init() {
+        // Initialize your secured component here
+    }
+}
