af98c5ae64fec82f2a18d94bf1c48a80bbf1d4c6
Leif Åstrand <leif@vaadin.com>
2019-05-06 11:40:09 +0300

    Document JavaScript return values (#593)
    * Document JavaScript return values

--- documentation/src/main/java/com/vaadin/flow/tutorial/webcomponent/PaperSlider.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/webcomponent/PaperSlider.java
@@ -6,7 +6,6 @@ import com.vaadin.flow.component.PropertyDescriptor;
 import com.vaadin.flow.component.PropertyDescriptors;
 import com.vaadin.flow.component.Synchronize;
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.shared.Registration;
@@ -37,7 +36,7 @@ public class PaperSlider
     }
 
     public void increment() {
-        getElement().callFunction("increment");
+        getElement().callJsFunction("increment");
     }
 
     @Override
