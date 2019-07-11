86d0b7999e9521b02beb2316dc7b413b0e8d03fd
Manuel Carrasco Moñino <manolo@apache.org>
2019-07-11 15:42:45 +0200

    Introduce @CssImport in theme documentation (#672)

--- documentation/src/main/java/com/vaadin/flow/tutorial/theme/ApplicationThemingBasics.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/theme/ApplicationThemingBasics.java
@@ -1,6 +1,6 @@
 package com.vaadin.flow.tutorial.theme;
 
-import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.router.RouterLayout;
@@ -9,7 +9,7 @@ import com.vaadin.flow.tutorial.annotations.CodeFor;
 @CodeFor("theme/application-theming-basics.asciidoc")
 public class ApplicationThemingBasics {
 
-    @JsModule("./styles/shared-styles.js")
+    @CssImport("./styles/shared-styles.css")
     @Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
     public class MainLayout extends Div implements RouterLayout {
     }
