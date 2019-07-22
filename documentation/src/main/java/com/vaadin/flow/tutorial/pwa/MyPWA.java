72f008bf2a0a3b14071e66aa92412ee983c8a80a
Martin Vysny <martin@vysny.me>
2019-07-22 19:55:52 +0300

    Sources formatting fixes

--- documentation/src/main/java/com/vaadin/flow/tutorial/pwa/MyPWA.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/pwa/MyPWA.java
@@ -13,4 +13,20 @@ public class MyPWA extends Div {
     public MyPWA(){
         setText("Welcome to my PWA");
     }
+
+    @PWA(name = "My Progressive Web Application",
+            shortName = "MyPWA",
+            manifestPath = "manifest.json")
+    class MyPWA1 {}
+
+    @PWA(name = "My Progressive Web Application",
+            shortName = "MyPWA",
+            offlineResources = { "styles/offline.css",
+                    "js/jquery.js", "img/offline.jpg"})
+    class MyPWA2 {}
+
+    @PWA(name = "My Progressive Web Application",
+            shortName = "MyPWA",
+            iconPath = "img/icons/logo.png")
+    class MyPWA3 {}
 }
