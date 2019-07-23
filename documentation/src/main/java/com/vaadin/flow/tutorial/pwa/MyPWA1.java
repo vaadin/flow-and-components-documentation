5534c196a21bede601c4ce76bd7358ae18408dbd
Martin Vysny <martin@vysny.me>
2019-07-23 10:08:32 +0300

    Sources formatting fixes

--- documentation/src/main/java/com/vaadin/flow/tutorial/pwa/MyPWA1.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/pwa/MyPWA1.java
@@ -3,7 +3,7 @@ package com.vaadin.flow.tutorial.pwa;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 
-@CodeFor("pwa/tutorial-pwa-pwa-with-flow.asciidoc")
+@CodeFor("pwa/tutorial-pwa-icons.asciidoc")
 @PWA(name = "My Progressive Web Application",
         shortName = "MyPWA",
         iconPath = "img/icons/logo.png")
