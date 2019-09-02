908de446925de50b3e35f88a67d8b99e84f8b434
Pekka Hyvönen <pekka@vaadin.com>
2019-09-02 11:44:08 +0300

    Fix links

--- documentation/src/main/java/com/vaadin/flow/tutorial/migration/V14AddonMigration.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/migration/V14AddonMigration.java
@@ -5,7 +5,7 @@ import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 
-@CodeFor("v14-migration/v14-addon-migration-guide.asciidoc")
+@CodeFor("v14-migration/v14-addon-guide.asciidoc")
 public class V14AddonMigration {
 
     // other code omitted for clarity
