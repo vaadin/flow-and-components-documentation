58f7795b399bb1309bc59eac754dd02e438fa91d
Joni Uitto <joni@vaadin.com>
2019-11-20 11:54:27 +0200

    Documentation draft for portlet CDI
    - write doc + code
    - update overviews

---
+++ documentation/src/main/java/com/vaadin/flow/tutorial/portletsupport/CdiExample.java
@@ -0,0 +1,27 @@
+package com.vaadin.flow.tutorial.portletsupport;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.portal.VaadinPortlet;
+import com.vaadin.flow.tutorial.annotations.CodeFor;
+
+@CodeFor("portlet-support/portlet-07-cdi-support.asciidoc")
+public class CdiExample {
+    // old, normal portlet:
+    public class NormalBookListPortlet extends VaadinPortlet<BookListView> {
+        // ... contents
+    }
+
+    // new, cdi portlet:
+    public class CdiBookListPortlet extends VaadinCdiPortlet<BookListView> {
+        // ... contents
+    }
+
+    private static class BookListView extends Component {
+
+    }
+
+    // TODO: remove once the cdi support is in
+    private static class VaadinCdiPortlet<C extends Component> extends VaadinPortlet<C> {
+
+    }
+}
