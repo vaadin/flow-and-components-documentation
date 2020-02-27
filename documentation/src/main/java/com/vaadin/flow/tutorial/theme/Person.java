665b24b416b57eaf9cae7a70dcc29a4138b7462e
Anastasia Smirnova <anasmi@utu.fi>
2020-02-27 14:48:12 +0200

    Create Person.java

---
+++ documentation/src/main/java/com/vaadin/flow/tutorial/theme/Person.java
@@ -0,0 +1,38 @@
+package com.vaadin.flow.tutorial.theme;
+
+public class Person {
+
+    private int age;
+    private String firstName;
+    private String lastName;
+
+    public Person(int age, String firstName, String lastName) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
