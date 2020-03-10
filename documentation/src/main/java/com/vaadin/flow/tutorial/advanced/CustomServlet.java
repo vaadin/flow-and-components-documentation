6c3db7cead07f30102143ca4fa581e710c302420
haijian <haijian@vaadin.com>
2020-03-10 21:38:11 +0200

    remove vservlet init param aadin prefix for pnpm

--- documentation/src/main/java/com/vaadin/flow/tutorial/advanced/CustomServlet.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/advanced/CustomServlet.java
@@ -7,7 +7,7 @@ import com.vaadin.flow.server.VaadinServlet;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 
 @WebServlet(urlPatterns = "/*", name = "myservlet", asyncSupported = true, initParams = {
-        @WebInitParam(name = "vaadin.pnpm.enable", value = "true") })
+        @WebInitParam(name = "pnpm.enable", value = "true") })
 @CodeFor("advanced/tutorial-switch-npm-pnpm.asciidoc")
 public class CustomServlet extends VaadinServlet {
 }
