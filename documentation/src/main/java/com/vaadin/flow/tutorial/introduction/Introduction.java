17551ef43966f46e5080e19132696fc7648feb27
Jouni Koivuviita <jouni@vaadin.com>
2020-05-12 10:25:44 +0300

    Themes and styling docs update (#1027) (#1254)
    Complete rewrite of the theming and styling documentation.
    Adds the documentation for Lumo and Material theme custom properties.

--- documentation/src/main/java/com/vaadin/flow/tutorial/introduction/Introduction.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/introduction/Introduction.java
@@ -9,18 +9,18 @@ import com.vaadin.flow.tutorial.annotations.CodeFor;
 @CodeFor("introduction/introduction-overview.asciidoc")
 public class Introduction {
 
-    public Introduction() {
-    	// Create an HTML element
-    	Div layout = new Div();
+	public Introduction() {
+		// Create an HTML element
+		Div layout = new Div();
 
-    	// Use TextField for standard text input
-    	TextField textField = new TextField("Your name");
+		// Use TextField for standard text input
+		TextField textField = new TextField("Your name");
 
-    	// Button click listeners can be defined as lambda expressions
-    	Button button = new Button("Say hello",
-    	          e -> Notification.show("Hello!"));
+		// Button click listeners can be defined as lambda expressions
+		Button button = new Button("Say hello",
+				e -> Notification.show("Hello!"));
 
-    	// Add the web components to the HTML element
-    	layout.add(textField, button);
-    }
+		// Add the web components to the HTML element
+		layout.add(textField, button);
+	}
 }
