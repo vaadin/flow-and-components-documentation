7573d737af65231537f39497476432fbff054b97
Stepan Zolotarev <zolotaryov@gmail.com>
2020-07-02 09:25:36 +0300

    Introduce LitElement template docs (#1310)

--- documentation/src/main/java/com/vaadin/flow/tutorial/lit/SlotView.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/lit/SlotView.java
@@ -13,25 +13,21 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.flow.tutorial.polymer;
+package com.vaadin.flow.tutorial.lit;
 
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
+import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.dom.ElementFactory;
-import com.vaadin.flow.router.ParentLayout;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouterLayout;
-import com.vaadin.flow.templatemodel.TemplateModel;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 
 @CodeFor("polymer-templates/tutorial-template-components-in-slot.asciidoc")
-public class PolymerSlotView {
+public class SlotView {
+
     @Tag("component-container")
     @JsModule("./com/example/component-container.js")
-    public class ComponentContainer extends PolymerTemplate<TemplateModel> {
+    public class ComponentContainer extends LitTemplate {
 
         public ComponentContainer() {
             Element label = ElementFactory.createLabel("Main layout header");
@@ -43,7 +39,7 @@ public class PolymerSlotView {
 
     @Tag("name-element")
     @JsModule("./com/example/name-element.js")
-    public class NameElement extends PolymerTemplate<TemplateModel> {
+    public class NameElement extends LitTemplate {
         public NameElement() {
             Element firstName = ElementFactory.createSpan("Jack");
             Element middleName = ElementFactory.createSpan(" James");
