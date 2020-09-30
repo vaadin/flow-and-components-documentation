33ae4c872f36c7ba89b8fbb3fecde67102e2e1cd
Haijian Wang <30408303+haijian-vaadin@users.noreply.github.com>
2020-09-30 21:37:10 +0300

    feat: add docs for offline form submission (#1356)

---
+++ documentation/src/main/java/com/vaadin/flow/tutorial/pwa/DeferrableEndpoint.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.flow.tutorial.pwa;
+
+import com.vaadin.flow.server.connect.Deferrable;
+import com.vaadin.flow.server.connect.Endpoint;
+import com.vaadin.flow.tutorial.annotations.CodeFor;
+
+@CodeFor("pwa/tutorial-defer-endpoint-request.asciidoc")
+public class DeferrableEndpoint {
+
+    @Endpoint
+    public class PersonEndpoint {
+        public Person loadPerson() {
+            return new Person();
+        }
+
+        /**
+         * Saves the edited Person from the view.
+         * @param person form data to save
+         */
+        @Deferrable
+        public void savePerson(Person person) {
+            // ...
+        }
+    }
+
+    class Person {
+        // ...
+    }
+}
