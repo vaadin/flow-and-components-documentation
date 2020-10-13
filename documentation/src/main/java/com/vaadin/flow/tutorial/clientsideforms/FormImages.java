94890961857be8e62576f69617c32a37e159defd
Johannes Eriksson <joheriks@vaadin.com>
2020-10-13 18:11:22 +0300

    doc: provide example of using `vaadin-upload` for form image file uploads

---
+++ documentation/src/main/java/com/vaadin/flow/tutorial/clientsideforms/FormImages.java
@@ -0,0 +1,41 @@
+package com.vaadin.flow.tutorial.clientsideforms;
+
+import com.vaadin.flow.server.connect.Endpoint;
+import com.vaadin.flow.tutorial.annotations.CodeFor;
+
+@CodeFor("client-side-forms/appendix-form-images.asciidoc")
+public class FormImages {
+    /**
+     * Contact card with base64-encoding image
+     */
+    public class Contact {
+        // ...
+
+        public String getAvatarBase64() {
+            // ...
+            return "";
+        }
+
+        public void setAvatarBase64(String avatarBase64) {
+            // ...
+        }
+
+        public String getFullName() {
+            // ...
+            return "";
+        }
+
+        public void setFullName(String fullName) {
+            // ...
+        }
+    }
+
+    @Endpoint
+    public class ServiceEndpoint {
+        // ...
+
+        public void saveContact(Contact contact) {
+            // ...
+        }
+    }
+}
