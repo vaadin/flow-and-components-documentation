d37e208cfca95708a0e12f528bb63d1ecbca6537
Anton Platonov <anton@vaadin.com>
2020-12-11 10:29:51 +0200

    doc(offline): update documentation for PWA offline start features (#1405)
    * doc(offline): update documentation for PWA offline start features
    
    Fixes #9065
    
    * fix: better wording
    
    * fix review comments
    
    * fix relevant Vale warnings

--- documentation/src/main/java/com/vaadin/flow/tutorial/pwa/MyPWA4.java
+++ documentation/src/main/java/com/vaadin/flow/tutorial/pwa/MyPWA4.java
@@ -4,12 +4,12 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.tutorial.annotations.CodeFor;
 
-@CodeFor("pwa/tutorial-pwa-web-app-manifest.asciidoc")
-public class MyPWA3 {
+@CodeFor("pwa/tutorial-pwa-offline.asciidoc")
+public class MyPWA4 {
 
     @PWA(name = "My Progressive Web Application",
          shortName = "MyPWA",
-         manifestPath = "mypwa_manifest.json")
+         offlinePath = "offline.html")
     public class AppShell implements AppShellConfigurator {
     }
 }
